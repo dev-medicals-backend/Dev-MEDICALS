f8d6e02764d52f7e471f2b421a254ac2358e04fb
dev-medicals-backend <abhisheksahani480@gmail.com>
2025-11-20 12:48:07 +0530

    Create app/api/admin/login/route.js

---
+++ app/api/admin/login/route.js
@@ -0,0 +1,12 @@
+export async function POST(req) {
+  const { username, password } = await req.json();
+
+  if (
+    username === process.env.ADMIN_USERNAME &&
+    password === process.env.ADMIN_PASSWORD
+  ) {
+    return Response.json({ success: true });
+  }
+
+  return Response.json({ success: false, message: "Invalid login" });
+}
