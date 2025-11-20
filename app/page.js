f54f6bad585c5ce5f8235677c25010b5807255a4
dev-medicals-backend <abhisheksahani480@gmail.com>
2025-11-20 12:37:00 +0530

    Create app/page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Dev Medicals</h1>
+      <p>Welcome to your medicine store website.</p>
+    </main>
+  );
+}
